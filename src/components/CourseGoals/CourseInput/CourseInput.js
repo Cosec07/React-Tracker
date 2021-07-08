273f98f3e999f390fe70e111ff815f20b561135f
steevesm <steevesm@sheridancollege.ca>
2021-07-08 15:42:38 -0400

    Update CourseInput.js

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -52,7 +52,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div class={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
